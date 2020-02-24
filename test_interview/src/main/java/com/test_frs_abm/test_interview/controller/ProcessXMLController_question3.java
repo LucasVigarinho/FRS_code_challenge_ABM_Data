816cd26c23094f34e0a8234642adba0257825f9b
LucasVigarinho <lucas.maximo.mv@gmail.com>
2020-02-24 23:26:19 +0000

    Commit @LucasVigarinho ABM_Developer_Test README MODIFICATION and STRUCTURE

--- test_interview/src/main/java/com/test_frs_abm/test_interview/controller/ProcessXMLController_question3.java
+++ test_interview/src/main/java/com/test_frs_abm/test_interview/controller/ProcessXMLController_question3.java
@@ -2,21 +2,22 @@ package com.test_frs_abm.test_interview.controller;
 
 import com.test_frs_abm.test_interview.service.ProcessXMLService_Question3;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProcessXMLController {
+public class ProcessXMLController_question3 {
 
-//    @GetMapping(path = "/test")
-//    public ResponseEntity getTest() {
-//            return ResponseEntity.status(200).body ("Status: 0");
-////            return ResponseEntity.status(400).body ("Status: -1");
-////            return ResponseEntity.status(400).body ("Status: -2");
-//
-////        return "Test response";
-//    }
+    @GetMapping(path = "/test")
+    public ResponseEntity getTest() {
+            return ResponseEntity.status(200).body ("Status: 0");
+//            return ResponseEntity.status(400).body ("Status: -1");
+//            return ResponseEntity.status(400).body ("Status: -2");
+
+//        return "Test response";
+    }
 
     @PostMapping(path = "/test")
     public ResponseEntity getTest(@RequestBody String body) {
